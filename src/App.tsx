9e7e0474af4b55ec1583c2b560183068c9dc2c9e
lamxuanbao <lamxuanbao.tb@gmail.com>
2024-06-04 13:22:24 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './routes';
-import { useMount } from 'ahooks';
-import { setLayout } from './utils/Layout';
 
 function App() {
-  useMount(() => {
-    setLayout();
-  });
   return (
     <>
       <RouterProvider router={router} />
